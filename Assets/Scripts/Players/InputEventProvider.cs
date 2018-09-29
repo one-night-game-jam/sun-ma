e34fa7f9162319fb72580ae8d2f1c2dbf6baf547
Shota HAMADA <maku693@outlook.com>
2018-09-29 13:11:56 +0900

    Add InputEventProvider

---
+++ Assets/Scripts/Players/InputEventProvider.cs
@@ -0,0 +1,23 @@
+using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Players
+{
+    public class InputEventProvider : UIBehaviour
+    {
+        public IObservable<Vector2> OnPullAsObservable()
+        {
+            return this.OnDragAsObservable()
+                .Select(x => x.position - x.pressPosition);
+        }
+
+        public IObservable<Vector2> OnEndPullAsObservable()
+        {
+            return this.OnEndDragAsObservable()
+                .WithLatestFrom(this.OnPullAsObservable(), (_, x) => x);
+        }
+    }
+}
