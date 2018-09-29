80773c4cf0d36abd83b9aa4b4abb8e99771888eb
Shoma SATO <noir.neo.04@gmail.com>
2018-09-29 15:11:40 +0900

    Hit dish and freeze

---
+++ Assets/Scripts/Objects/Dishes/DishCore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sanmas;
+using UniRx;
+using UnityEngine;
+
+namespace Objects.Dishes
+{
+    public class DishCore : MonoBehaviour, ISanmaEnterHandler
+    {
+        readonly ISubject<SanmaCore> _enter = new Subject<SanmaCore>();
+
+        void Start()
+        {
+            _enter.Subscribe(x => x.Freeze())
+                .AddTo(this);
+        }
+
+        void ISanmaEnterHandler.OnEnter(SanmaCore sanmaCore)
+        {
+            _enter.OnNext(sanmaCore);
+        }
+    }
+}
