db811f71fa9e2775525f53130f9979c2c161d4f9
Shoma SATO <noir.neo.04@gmail.com>
2018-09-29 13:17:45 +0900

    Add sanma mover

--- Assets/Scripts/Sanmas/SanmaCore.cs
+++ Assets/Scripts/Sanmas/SanmaCore.cs
@@ -1,9 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UniRx;
+using UnityEngine;
 
 namespace Sanmas
 {
     public class SanmaCore : MonoBehaviour
     {
-        
+        readonly AsyncSubject<Vector2> _launch = new AsyncSubject<Vector2>();
+        public IObservable<Vector2> OnLaunchAsObservable => _launch;
+
+        Vector2 _gravity;
+        public Vector2 Gravity => _gravity;
+
+        public void Launch(Vector2 velocity)
+        {
+            _launch.OnNext(velocity);
+            _launch.OnCompleted();
+        }
+
+        public void UpdateGravity(Vector2 gravity)
+        {
+            _gravity = gravity;
+        }
     }
 }
---
+++ Assets/Scripts/Sanmas/SanmaMover.cs
@@ -0,0 +1,29 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Sanmas
+{
+    public class SanmaMover : MonoBehaviour
+    {
+        [SerializeField] SanmaCore _core;
+        [SerializeField] Rigidbody2D _rigidbody;
+
+        void Start()
+        {
+            _core.OnLaunchAsObservable
+                .ObserveOn(Scheduler.MainThreadFixedUpdate)
+                .Subscribe(Launch)
+                .AddTo(this);
+        }
+
+        void Launch(Vector2 force)
+        {
+            _rigidbody.AddForce(force, ForceMode2D.Impulse);
+        }
+
+        void FixedUpdate()
+        {
+            _rigidbody.AddForce(_core.Gravity, ForceMode2D.Force);
+        }
+    }
+}
