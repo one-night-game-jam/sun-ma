37e82fa7c1b6b8bb16479812a665ac97e1e67684
Shoma SATO <noir.neo.04@gmail.com>
2018-09-29 11:54:31 +0900

    Load level and result from game core

---
+++ Assets/Scripts/Scenes/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UniRx.Async;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scenes
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        [SerializeField]
+        string[] sceneNames;
+
+        async void Start()
+        {
+            var loaded = Enumerable
+                .Select(Enumerable.Range(0, SceneManager.sceneCount), SceneManager.GetSceneAt)
+                .Select(s => s.name);
+            await LoadScenesAdditive(sceneNames.Except(loaded));
+        }
+
+        static async UniTask LoadScenesAdditive(IEnumerable<string> sceneNames)
+        {
+            await UniTask.WhenAll(sceneNames.Select(LoadSceneAdditive));
+        }
+
+        static async UniTask LoadSceneAdditive(string sceneName)
+        {
+            await SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        }
+    }
+}
