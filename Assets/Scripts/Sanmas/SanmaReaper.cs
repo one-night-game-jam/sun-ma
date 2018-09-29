f1441a7edbe615ae51c7c0c8bb41e27dad79c05f
Shoma SATO <noir.neo.04@gmail.com>
2018-09-29 13:28:38 +0900

    Add sanma reaper

---
+++ Assets/Scripts/Sanmas/SanmaReaper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Sanmas
+{
+    public class SanmaReaper : MonoBehaviour
+    {
+        [SerializeField] float _lifetimeSeconds;
+
+        void Start()
+        {
+            Observable.Timer(TimeSpan.FromSeconds(_lifetimeSeconds))
+                .Subscribe(_ => Destroy(gameObject))
+                .AddTo(this);
+        }
+    }
+}
