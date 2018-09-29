fde95081b70089c42ee6090dfd6b0f6219a9f385
Shota HAMADA <maku693@outlook.com>
2018-09-29 22:39:57 +0900

    Setup tutorial

---
+++ Assets/Scripts/Players/ToggleTutorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToggleTutorial : UIBehaviour
+{
+    [SerializeField]
+    GameObject tutorialRoot;
+
+    private void Start()
+    {
+        this.OnPointerDownAsObservable()
+            .Subscribe(_ => tutorialRoot.SetActive(false))
+            .AddTo(this);
+    }
+}
