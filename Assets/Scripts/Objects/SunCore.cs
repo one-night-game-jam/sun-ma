f46d394ae7cd548cd01e96258db2c0ba4c628ae6
Shota HAMADA <maku693@outlook.com>
2018-09-29 15:23:12 +0900

    Add the sun

---
+++ Assets/Scripts/Objects/SunCore.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Sanmas;
+
+namespace Objects
+{
+    public class SunCore : MonoBehaviour, ISanmaStayHandler, ISanmaExitHandler
+    {
+        [SerializeField]
+        private float gravity;
+
+        public void OnStay(SanmaCore sanmaCore)
+        {
+            sanmaCore.UpdateGravity((transform.position - sanmaCore.transform.position) * gravity);
+        }
+
+        public void OnExit(SanmaCore sanmaCore)
+        {
+            sanmaCore.UpdateGravity(Vector2.zero);
+        }
+    }
+}
