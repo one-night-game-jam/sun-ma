f36c053c87f1c3a15fa458cb8d12b1797cecb827
Shoma SATO <noir.neo.04@gmail.com>
2018-10-05 01:27:26 +0900

    Add acknowledgements window

---
+++ Assets/Scripts/UIs/Common/OpenButton.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIs.Common
+{
+    public class OpenButton : MonoBehaviour
+    {
+        [SerializeField]
+        Button button;
+
+        [SerializeField]
+        GameObject prefab;
+
+        void Start()
+        {
+            button.OnClickAsObservable()
+                .Subscribe(_ => Instantiate(prefab))
+                .AddTo(this);
+        }
+    }
+}
