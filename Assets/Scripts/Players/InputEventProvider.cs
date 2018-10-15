6d662446b797bb076a2ccd4040b6c9b8603f83bc
Shoma SATO <noir.neo.04@gmail.com>
2018-10-16 03:20:12 +0900

    マルチタップ反応しないように

--- Assets/Scripts/Players/InputEventProvider.cs
+++ Assets/Scripts/Players/InputEventProvider.cs
@@ -11,12 +11,15 @@ namespace Players
         public IObservable<Vector2> OnPullAsObservable()
         {
             return this.OnDragAsObservable()
+                // When using a mouse the pointerId returns -1, -2, or -3.
+                .Where(x => x.pointerId == -1 || x.pointerId == 0)
                 .Select(x => (x.position - x.pressPosition) / Screen.dpi);
         }
 
         public IObservable<Vector2> OnEndPullAsObservable()
         {
             return this.OnEndDragAsObservable()
+                .Where(x => x.pointerId == -1 || x.pointerId == 0)
                 .WithLatestFrom(this.OnPullAsObservable(), (_, x) => x);
         }
     }
