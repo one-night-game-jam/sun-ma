7b5447a41f6c2d55f73c570e905ae086226fda64
Shoma SATO <noir.neo.04@gmail.com>
2018-10-16 01:20:43 +0900

    Add initial scene
    最初に読み込むシーンで canvas の描画が遅れるため

---
+++ Assets/Scripts/Scenes/LoadSceneImmediately.cs
@@ -0,0 +1,16 @@
+﻿using UniRx.Async;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scenes
+{
+    public class LoadSceneImmediately : MonoBehaviour
+    {
+        [SerializeField] string sceneName;
+
+        async void Awake()
+        {
+            await SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
