22acb0dda67876776a9104bb997a5a23450a9c2e
Shoma SATO <noir.neo.04@gmail.com>
2018-10-18 01:51:23 +0900

    Add link to privacy policy

---
+++ Assets/Scripts/UIs/Common/LinkButton.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIs.Common
+{
+    public class LinkButton : MonoBehaviour
+    {
+        [SerializeField]
+        Button button;
+
+        [SerializeField]
+        string url;
+
+        void Start()
+        {
+            button.OnClickAsObservable()
+                .Subscribe(_ => Application.OpenURL(url))
+                .AddTo(this);
+        }
+    }
+}
