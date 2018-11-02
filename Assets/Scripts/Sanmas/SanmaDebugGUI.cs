c30b9edc4659a86be0b13bf1931a19ca60d7ad03
Shoma SATO <noir.neo.04@gmail.com>
2018-11-02 22:51:26 +0900

    Debug gui

---
+++ Assets/Scripts/Sanmas/SanmaDebugGUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Sanmas
+{
+    public class SanmaDebugGUI : MonoBehaviour
+    {
+        [SerializeField] SanmaCore _core;
+        GUIStyle _guiStyle;
+
+        void Awake()
+        {
+            _guiStyle = new GUIStyle
+            {
+                fontSize = 48,
+                normal = new GUIStyleState
+                {
+                    textColor = Color.white
+                }
+            };
+        }
+
+        void OnGUI()
+        {
+            var screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            GUI.Label(new Rect (screenPoint.x - 50, Screen.height - screenPoint.y - 150, 200, 100), $"{_core.BroiledValue.Value*200:F2}%", _guiStyle);
+
+        }
+    }
+}
